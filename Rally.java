0408886df196c01c5b0ebde3924829c5f95fb7b8
huxflux <johnis@student.hist.no>
2013-09-20 13:45:57 +0200

    test

--- Rally.java
+++ Rally.java
@@ -12,29 +12,42 @@ import lejos.nxt.*;
 public class Rally {
 	public static void main(String[] args) throws Exception {
 
-		final int 		MAX_SPEED = 900;
+		final int 		MAX_SPEED = 400;
 		final int 		LIGHT_THRESHOLD = 45;
 
 		LightSensor lightSensor = new LightSensor(SensorPort.S3);
+		TouchSensor touchSensor1 = new TouchSensor(SensorPort.S1);
+		TouchSensor touchSensor4 = new TouchSensor(SensorPort.S4);
+
 		Thread.sleep(1000);
 
-		while (!Button.ENTER.isDown()) {
-			Motor.A.setSpeed(MAX_SPEED * 2 / 3);
-			Motor.B.setSpeed(MAX_SPEED);
+		Motor.A.setSpeed(MAX_SPEED * 3 / 4);
+		Motor.B.setSpeed(MAX_SPEED);
 
-			Motor.A.backward();
-			Motor.B.backward();
+		Motor.A.backward();
+		Motor.B.backward();
+
+		while (!Button.ENTER.isDown()) {
 
 			if (lightSensor.readValue() < LIGHT_THRESHOLD) {
-//				Motor.A.setSpeed(0);
 				Motor.B.setSpeed(0);
-				Thread.sleep(100);
-//				Motor.B.setSpeed(MAX_SPEED);
-//				Thread.sleep(1000);
+				Thread.sleep(300);
+				Motor.B.setSpeed(MAX_SPEED);
+				Motor.B.backward();
+			}
+
+			if (touchSensor1.isPressed() || touchSensor4.isPressed()) {
+				Motor.A.forward();
+				Motor.B.forward();
+				Thread.sleep(250);
+				Motor.B.setSpeed(0);
+				Motor.A.backward();
+				Thread.sleep(500);
+				Motor.B.setSpeed(MAX_SPEED);
+				Motor.B.backward();
 
 
 			}
-
 		}
 	}
 }
